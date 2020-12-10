d9c1f9a3c4a71422dd8e6f8f057600df31323dd5
Samdan9 <63875077+Samdan9@users.noreply.github.com>
2020-12-09 17:44:37 -0800

    Add files via upload

---
+++ unblocked/minecraft_files/minecraft.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.2 local-build-20201209
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('minecraft_files/minecraftMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');detectBrowser();Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
